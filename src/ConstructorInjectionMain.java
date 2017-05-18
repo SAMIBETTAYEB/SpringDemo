8557552b60e801e42aeea5991642cb8783eea475
SAMI BETTAYEB <SAMI BETTAYEB>
2017-05-18 23:36:11 +0200

    1.0.1

---
+++ src/ConstructorInjectionMain.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Bettayeb on 5/18/2017.
+ */
+public class ConstructorInjectionMain {
+    public static void main(String[] args){
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        System.out.println(((Person)context.getBean("person1")).toString());
+    }
+}
