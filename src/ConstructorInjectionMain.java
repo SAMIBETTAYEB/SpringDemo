f09689f5d03211dcf67383105c91440eabcea081
SAMI BETTAYEB <SAMI BETTAYEB>
2017-05-18 23:55:57 +0200

    1.0.4

--- src/ConstructorInjectionMain.java
+++ src/ConstructorInjectionMain.java
@@ -7,7 +7,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ConstructorInjectionMain {
     public static void main(String[] args){
         ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        System.out.println("Using constructor-arg (default):");
         System.out.println(((Person)context.getBean("person1")).toString());
+        System.out.println("Using constructor-arg type:");
         System.out.println(((Person)context.getBean("person2")).toString());
+        System.out.println("Using constructor-arg index:");
+        System.out.println(((Person)context.getBean("person3")).toString());
     }
 }
